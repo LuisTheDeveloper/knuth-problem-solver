b86feb1b718128405632254f2f4ef6fcba39c86e
Pedro The Dev <47012647+LuisTheDeveloper@users.noreply.github.com>
2021-02-19 11:00:12 +0000

    Create Arrays-JS.js

---
+++ Arrays-JS.js
@@ -0,0 +1,29 @@
+/*
+Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+Find all the elements that appear twice in this array.
+Could you do it without extra space and in O(n) runtime?
+
+The important point is: 1 ≤ a[i] ≤ n (n = size of array), so we can use the value of each element in the array as an index,
+to optimize the final solution in O(n) time complexity without any increase in space complexity apart from the results array.
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ 
+var findDuplicates = function(nums) {
+    let result = []
+    
+    arrLength = nums.length
+    
+    for(let i=0; i<arrLength; i++) {
+        let index = Math.abs(nums[i]) -1
+        if(nums[index] < 0) {
+            result.push(Math.abs(nums[i]))
+        } else {
+            nums[index] *= -1
+        }
+    }
+    
+    return result
+};
