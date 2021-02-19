f48b849e63efc02e20a49ea07e3e54795b04d554
Pedro The Dev <47012647+LuisTheDeveloper@users.noreply.github.com>
2021-02-19 11:02:59 +0000

    Update Arrays-JS.js

--- Arrays-JS.js
+++ Arrays-JS.js
@@ -1,4 +1,9 @@
+// Arrays problems and solutions with an optimal solution as possible
+
+
 /*
+FIND DUPLICATES
+
 Given an array of integers, 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
 Find all the elements that appear twice in this array.
 Could you do it without extra space and in O(n) runtime?
